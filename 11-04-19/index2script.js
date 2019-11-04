6478e82f997e87f6fee057fc9f45441e40819f71
Swaroop Raja Marni <swaroop.marni@gmail.com>
2019-11-04 12:36:55 -0700

    Create index2script.js

---
+++ 11-04-19/index2script.js
@@ -0,0 +1,71 @@
+//Please do un comment Every section before executing it 
+
+//Alert ----------
+
+//alert("Hello I am from External JS File");
+
+// variables -----------
+
+// var name="John";
+// var age=25;
+
+// document.write("Hello "+name+", Your Age: "+age);
+
+// var greeting= "Hello";
+// //  Indexing   01234
+
+// document.write(greeting.length +"<br>");
+// document.write(greeting.charAt(0));
+
+//Getting User Input -----------------
+
+// var name=window.prompt("Enter your name: ");
+// alert("Your Name is: "+name);
+
+// var num1= window.prompt("Enter First Number: ");
+// var num2= window.prompt("Enter Second Number: ");
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+// document.write(num1+num2);
+
+//Arrays-------------
+
+
+// var fruits=["Apples","Oranges","Bananas"];
+// // index       0        1         2
+
+// document.write(fruits + "<br>");
+// document.write(fruits[0] + "<br>");
+// document.write(fruits.length + "<br>");
+
+// fruits[0]="Peaches";
+// document.write(fruits + "<br>");
+
+//Functions ----------------------------
+
+
+// function sayHi(name,age){
+// 	//var name="John";
+// 	document.write("Hello "+name+", your age:"+age);
+// 	document.write("<br>");
+// }
+
+// sayHi("Raja",25);
+// sayHi("John",30);
+
+
+function myFunction(){
+  document.querySelector("h1").style.color = "green";
+}
+
+
+
+
+
+
+
+
+
+
+
+
